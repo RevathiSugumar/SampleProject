d27fc968c30d1d33163c71e11a411953784fb5e9
RevathiSugumar <revathisugumar1992@gmail.com>
2021-11-22 13:21:34 +0545

    samplegit

---
+++ src/test/java/org/sample/Sample.java
@@ -0,0 +1,13 @@
+package org.sample;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Sample {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Venkat\\eclipse-workspace\\TestProj\\driver\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.facebook.com");
+	}
+
+}
